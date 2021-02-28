4afd9d1255d979127058a376d6b9d0f4411bfe23
Reimu NotMoe <34613827+ReimuNotMoe@users.noreply.github.com>
2021-02-28 23:48:40 +0800

    Support STM32 + gcc

--- PortableEndian.h
+++ PortableEndian.h
@@ -12,26 +12,57 @@
 
 #if (defined(_WIN16) || defined(_WIN32) || defined(_WIN64)) && !defined(__WINDOWS__)
 
-#	define __WINDOWS__
+#define __WINDOWS__
+
+#endif
+
+#if (defined(STM32F0) || defined(STM32F1) || defined(STM32F2) || defined(STM32F3) || defined(STM32F4) || defined(STM32F7) || \
+defined(STM32G0) || defined(STM32G4) || defined(STM32H7) || defined(STM32L0) || defined(STM32L1) || defined(STM32L4) ||      \
+defined(STM32L4P) || defined(STM32L5) || defined(STM32MP1) || defined(STM32WL) || defined(STM32WB)) && !defined(__STM32__)
+
+#define __STM32__
 
 #endif
 
 #if defined(ESP_PLATFORM)
 
-#       define _LITTLE_ENDIAN LITTLE_ENDIAN
-#       define __bswap16     __bswap_16
-#       define __bswap32     __bswap_32
-#       define __bswap64     __bswap_64
+#define _LITTLE_ENDIAN LITTLE_ENDIAN
+#define __bswap16     __bswap_16
+#define __bswap32     __bswap_32
+#define __bswap64     __bswap_64
 
-#	include <endian.h>
+#include <endian.h>
+
+#elif defined(__STM32__)
+
+#if defined(__GNUC__) || defined(__clang__)
+
+#define htobe16(x) __builtin_bswap16(x)
+#define htole16(x) (x)
+#define be16toh(x) __builtin_bswap16(x)
+#define le16toh(x) (x)
+
+#define htobe32(x) __builtin_bswap32(x)
+#define htole32(x) (x)
+#define be32toh(x) __builtin_bswap32(x)
+#define le32toh(x) (x)
+
+#define htobe64(x) __builtin_bswap64(x)
+#define htole64(x) (x)
+#define be64toh(x) __builtin_bswap64(x)
+#define le64toh(x) (x)
+
+#else
+#error platform not supported
+#endif
 
 #elif defined(__linux__) || defined(__CYGWIN__)
 
-#	include <endian.h>
+#include <endian.h>
 
 #elif defined(__APPLE__)
 
-#	include <libkern/OSByteOrder.h>
+#include <libkern/OSByteOrder.h>
 
 #	define htobe16(x) OSSwapHostToBigInt16(x)
 #	define htole16(x) OSSwapHostToLittleInt16(x)
@@ -55,11 +86,11 @@
 
 #elif defined(__OpenBSD__)
 
-#	include <sys/endian.h>
+#include <sys/endian.h>
 
 #elif defined(__NetBSD__) || defined(__FreeBSD__) || defined(__DragonFly__)
 
-#	include <sys/endian.h>
+#include <sys/endian.h>
 
 #	define be16toh(x) betoh16(x)
 #	define le16toh(x) letoh16(x)
@@ -77,37 +108,37 @@
 # if defined(_MSC_VER)
 #   include <stdlib.h>
 
-#   define htobe16(x) _byteswap_ushort(x)
-#   define htole16(x) (x)
-#   define be16toh(x) _byteswap_ushort(x)
-#   define le16toh(x) (x)
+#define htobe16(x) _byteswap_ushort(x)
+#define htole16(x) (x)
+#define be16toh(x) _byteswap_ushort(x)
+#define le16toh(x) (x)
 
-#   define htobe32(x) _byteswap_ulong(x)
-#   define htole32(x) (x)
-#   define be32toh(x) _byteswap_ulong(x)
-#   define le32toh(x) (x)
+#define htobe32(x) _byteswap_ulong(x)
+#define htole32(x) (x)
+#define be32toh(x) _byteswap_ulong(x)
+#define le32toh(x) (x)
 
-#   define htobe64(x) _byteswap_uint64(x)
-#   define htole64(x) (x)
-#   define be64toh(x) _byteswap_uint64(x)
-#   define le64toh(x) (x)
+#define htobe64(x) _byteswap_uint64(x)
+#define htole64(x) (x)
+#define be64toh(x) _byteswap_uint64(x)
+#define le64toh(x) (x)
 
 # elif defined(__GNUC__) || defined(__clang__)
 
-#   define htobe16(x) __builtin_bswap16(x)
-#   define htole16(x) (x)
-#   define be16toh(x) __builtin_bswap16(x)
-#   define le16toh(x) (x)
+#define htobe16(x) __builtin_bswap16(x)
+#define htole16(x) (x)
+#define be16toh(x) __builtin_bswap16(x)
+#define le16toh(x) (x)
 
-#   define htobe32(x) __builtin_bswap32(x)
-#   define htole32(x) (x)
-#   define be32toh(x) __builtin_bswap32(x)
-#   define le32toh(x) (x)
+#define htobe32(x) __builtin_bswap32(x)
+#define htole32(x) (x)
+#define be32toh(x) __builtin_bswap32(x)
+#define le32toh(x) (x)
 
-#   define htobe64(x) __builtin_bswap64(x)
-#   define htole64(x) (x)
-#   define be64toh(x) __builtin_bswap64(x)
-#   define le64toh(x) (x)
+#define htobe64(x) __builtin_bswap64(x)
+#define htole64(x) (x)
+#define be64toh(x) __builtin_bswap64(x)
+#define le64toh(x) (x)
 
 #   else
 #     error platform not supported
